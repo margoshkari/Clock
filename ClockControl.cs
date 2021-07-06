199f7d83a3f54515842565cc52592b8350f5675e
Margo <margoshka.ri6@gmail.com>
2021-07-06 22:57:56 +0300

    Added events

--- ClockControl.cs
+++ ClockControl.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,8 +8,34 @@ using System.Windows.Forms;
 
 namespace Clock
 {
-    class ClockControl : Control
+    public class ClockControl : Control
     {
+        public Timer timer;
+        public ClockControl()
+        {
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Enabled = true;
+            timer.Tick += Timer_Tick;
+            timer.Start();
 
+            this.Paint += ClockControl_Paint;
+        }
+
+        public ClockControl(Size size, Point point)
+        {
+            this.Size = size;
+            this.Location = point;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ClockControl_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
     }
 }
